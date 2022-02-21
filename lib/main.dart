ef4197d1d8ff66231c2dfbfca8b9a06dbae0bd0b
cieldinoegg <cieldinoegg@naver.com>
2022-02-21 12:30:23 +0900

    일단 합쳐본 권한이랑 블로그

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
   runApp(MyApp());
@@ -33,7 +37,7 @@ class MyApp extends StatelessWidget {
             future: callPermissions(),
             builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
               if (snapshot.data) {
-                
+                 return WebViewExample();
               }
               return CircularProgressIndicator();
             },
@@ -44,3 +48,44 @@ class MyApp extends StatelessWidget {
   }
 }
 
+
+class WebViewExample extends StatefulWidget {
+  @override
+  WebViewExampleState createState() => WebViewExampleState();
+}
+
+class WebViewExampleState extends State<WebViewExample> {
+  WebViewController? _webViewController;
+
+  @override
+  void initState() {
+    super.initState();
+    // Enable hybrid composition.
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () => _goBack(context),
+      child: Scaffold(
+        body: WebView(
+          initialUrl: 'https://blog.naver.com/cieldinoegg',
+          javascriptMode: JavascriptMode.unrestricted,
+        ),
+      ),
+    );
+  }
+
+  Future<bool> _goBack(BuildContext context) async {
+    if (_webViewController == null) {
+      return true;
+    }
+    if (await _webViewController!.canGoBack()) {
+      _webViewController!.goBack();
+      return Future.value(false);
+    } else {
+      return Future.value(true);
+    }
+  }
+}
